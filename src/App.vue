c174ab85d3b664421872a884b53f3a11cca8ffe8
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-12 14:44:53 +0200

    [add]: Header as component+import in App

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <script>
+import Header from './components/Header.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header
+  }
+
 }
 </script>
 
 <template>
   <h1>Hello World</h1>
+
+  <Header />
+  
 </template>
 
 <style lang="scss">
 
 @use './scss/main.scss' as *;
-
-
 
 </style>
