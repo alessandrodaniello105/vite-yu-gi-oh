962e4342bfbbe1b5e5d6d30548f60d85e36a36a5
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-12 15:08:46 +0200

    [edit]: urlAPI in store.js

--- src/components/data/store.js
+++ src/components/data/store.js
@@ -1,6 +1,6 @@
 import { reactive } from 'vue';
 
 export const store = reactive({
-  urlAPI: 'https://db.ygoprodeck.com/api/v7/cardinfo.php',
+  urlAPI: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0',
   cardsList: []
 })
