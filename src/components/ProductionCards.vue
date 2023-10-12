4376b7cb449f4af78a55dc3b2e25ce57d3e48ce6
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-12 16:09:31 +0200

    [add]: props+template in ProductionCards

--- src/components/ProductionCards.vue
+++ src/components/ProductionCards.vue
@@ -1,13 +1,21 @@
 <script>
 export default {
-  name: 'ProductionCards'
+  name: 'ProductionCards',
+  props: {
+    name: String,
+    archetype: String,
+    id: Number,
+    image: String
+  }
 }
 </script>
 
 <template>
   <div class="col-3">
     <div class="card_ctm">
-      <img src="" alt="">
+      <img :src="image" :alt="name">
+      <h4>{{ name }}</h4>
+      <p>{{ archetype }}</p>
     </div>
   </div>
 </template>
