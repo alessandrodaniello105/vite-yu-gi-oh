397ca7beac6745d6a90664776d1e85a15970c2db
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-12 15:02:26 +0200

    [add]: Main, ProductionCards

---
+++ src/components/Main.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'Main'
+}
+</script>
+
+<template>
+  <main>MAIN</main>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/ProductionCards.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: 'ProductionCards'
+}
+</script>
+
+<template>
+  <div class="col-3">
+    <div class="card_ctm">
+      <img src="" alt="">
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
