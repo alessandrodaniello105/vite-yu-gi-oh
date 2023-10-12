bb6ba6cc4dd95d2f28f59a9922f7660e85c13aaf
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-12 17:14:07 +0200

    [add]: card style in ProductionCards

--- src/components/ProductionCards.vue
+++ src/components/ProductionCards.vue
@@ -13,13 +13,35 @@ export default {
 <template>
   <div class="col-3">
     <div class="card_ctm">
+
       <img :src="image" :alt="name">
-      <h4>{{ name }}</h4>
-      <p>{{ archetype }}</p>
+
+      <div class="text-box">
+        <h3 class="title">{{ name }}</h3>
+        <p>{{ archetype }}</p>
+      </div>
+
     </div>
   </div>
 </template>
 
 <style lang="scss">
+@use '../scss/partials/vars' as *;
 
+.card_ctm {
+  position: relative;
+  text-align: center;
+  background-color: $primary-color;
+  min-height: 550px;
+  .text-box {
+    padding: 5px;
+    p {
+      position: absolute;
+      bottom: 10px;
+      left: 50%;
+      transform: translateX(-50%);
+      font-size: 20px;
+    }
+  }
+}
 </style>
