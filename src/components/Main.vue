bc04e96bd2ad658107d7df3a722edd34dc6c7926
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-12 16:11:13 +0200

    [add]:ProductionCards/template container>row>cards

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,42 @@
 <script>
+import ProductionCards from "./ProductionCards.vue";
+import { store } from "./data/store.js";
+
 export default {
-  name: 'Main'
-}
+  name: "Main",
+  components: {
+    ProductionCards,
+  },
+  data() {
+    return {
+      store,
+    };
+  },
+  methods: {
+    testCycle() {
+      console.log(this.store.cardsList)
+    }
+  },
+  mounted() {
+    setTimeout(this.testCycle,1500)
+  }
+};
 </script>
 
 <template>
-  <main>MAIN</main>
+  <main>
+    <div class="container">
+      <div class="row">
+        <ProductionCards
+          v-for="card in store.cardsList"
+          :key="card.id"
+          :image="card.card_images.image_url"
+          :name="card.name"
+        />
+      </div>
+    </div>
+  </main>
 </template>
 
 <style lang="scss">
-
 </style>
