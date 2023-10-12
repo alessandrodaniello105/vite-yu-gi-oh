7381ff1360b5bae3dfe7425cb6b9c0e60b2e71b4
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-12 16:27:17 +0200

    [add]: img+title in Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,9 +5,17 @@ export default {
 </script>
 
 <template>
-  <header>HEADER</header>
+  <header>
+    <img src="#" alt="">
+    <h1>Yu-Gi-Oh API</h1>
+  </header>
 </template>
 
 <style lang="scss">
-
+header {
+  height: 100px;
+  background-color: #FFFFFF;
+  position: absolute;
+  top: 0;
+}
 </style>
